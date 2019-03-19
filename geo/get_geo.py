56b25de4ddbbb419d4aef87ceab8387299574625
redX li <redlixh001@gmail.com>
2019-03-20 00:34:01 +0800

    geo

--- geo/get_geo.py
+++ geo/get_geo.py
@@ -23,14 +23,6 @@ def get_geo(data, title, second_title, file_path):
 	geo.render(file_path)
 
 
-# data =[("海门", 9), ("鄂尔多斯", 12), ("招远", 12), ("舟山", 12), ("齐齐哈尔", 14), ("盐城", 15)]
-# geo =Geo("全国主要城市空气质量", "data from pm2.5", title_color="#fff", title_pos="center", width=1200, height=600, background_color='#404a59')
-# attr, value =geo.cast(data)
-# geo.add("", attr, value, type="effectScatter", is_random=True, effect_scale=5)
-# geo.show_config()
-# geo.render()
-
-
 def get_all_data(profession):
 	sql_str = "select * from lagoudata where company_jobs <> '[]'"
 	res = sql.queryall(sql_str)
